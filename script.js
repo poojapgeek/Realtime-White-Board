01b81af4fa6993ddd1e7ff405bb53eb4f47b9091
Pooja <poojahegde2003@gmail.com>
2024-05-19 15:58:33 +0530

    changes

--- script.js
+++ script.js
@@ -2,11 +2,27 @@
 let pencilElement = document.querySelector("#pencil");
 let eraserElement = document.querySelector("#eraser");
 let stickyElement = document.querySelector("#sticky");
-
 let uploadElement = document.querySelector("#upload");
 let downloadElement = document.querySelector("#download");
 let undoElement = document.querySelector("#undo");
 let redoElement = document.querySelector("#redo");
 
 //add event listener
-djhhgh;
+pencilElement.addEventListener("click", function tellPencil() {
+  console.log("pencil is clicked");
+});
+eraserElement.addEventListener("click", function tellEraser() {
+  console.log("Eraser is clicked");
+});
+stickyElement.addEventListener("click", function tellSticky() {
+  console.log("Sticky is clicked");
+});
+downloadElement.addEventListener("click", function tellDownload() {
+  console.log("Download is clicked");
+});
+undoElement.addEventListener("click", function tellUndo() {
+  console.log("undo is clicked");
+});
+redoElement.addEventListener("click", function tellRedo() {
+  console.log("redo is clicked");
+});
