41840653f10b2347ce4f6198a6b73d91f8741a83
Pooja P <152670256+poojapgeek@users.noreply.github.com>
2024-05-19 13:48:20 +0530

    Uploaded 1st prototype of Realtime White Board

---
+++ script.js
@@ -0,0 +1,11 @@
+//select the element
+let pencilElement = document.querySelector("#pencil");
+let eraserElement = document.querySelector("#eraser");
+let stickyElement = document.querySelector("#sticky");
+
+let uploadElement = document.querySelector("#upload");
+let downloadElement = document.querySelector("#download");
+let undoElement = document.querySelector("#undo");
+let redoElement = document.querySelector("#redo");
+
+//add event listener
