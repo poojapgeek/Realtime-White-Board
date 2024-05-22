5bc33ae515481ce3e5462da0d1331e52dbb8b2e4
Pooja <poojahegde2003@gmail.com>
2024-05-22 18:40:44 +0530

    pencil and eraser functionality added

--- script.js
+++ script.js
@@ -1,36 +1,73 @@
-//select the element
-
-let pencilElement = document.querySelector("#pencil");
-let eraserElement = document.querySelector("#eraser");
-let stickyElement = document.querySelector("#sticky");
-let uploadElement = document.querySelector("#upload");
-let downloadElement = document.querySelector("#download");
-let undoElement = document.querySelector("#undo");
-let redoElement = document.querySelector("#redo");
-
-//add event listener
-pencilElement.addEventListener("click", function tellPencil() {
-  console.log("pencil is clicked");
-});
-eraserElement.addEventListener("click", function tellEraser() {
-  console.log("Eraser is clicked");
-});
-stickyElement.addEventListener("click", function tellSticky() {
-  console.log("Sticky is clicked");
-});
-uploadElement.addEventListener("click", function tellUpload() {
-  console.log("Upload is clicked");
-});
-downloadElement.addEventListener("click", function tellDownload() {
-  console.log("Download is clicked");
-});
-undoElement.addEventListener("click", function tellUndo() {
-  console.log("undo is clicked");
-});
-redoElement.addEventListener("click", function tellRedo() {
-  console.log("redo is clicked");
-});
+// to get the canvas element from webpage
 let canvas = document.querySelector("#board");
+//assigning windows dimension to the canvas element dimension
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-ghggbcgfdgf;
+
+let tool = canvas.getContext("2d");
+
+let isDrawing = false; //flag
+
+//***************tool selector logic************************************
+let toolArr = document.querySelectorAll(".tool");
+let currentTool = "pencil";
+for (let i = 0; i < toolArr.length; i++) {
+  toolArr[i].addEventListener("click", function (e) {
+    const toolname = toolArr[i].id;
+    if (toolname == "pencil") {
+      currentTool = "pencil";
+      tool.strokeStyle = "blue";
+      console.log("pencil clicked");
+    } else if (toolname == "eraser") {
+      currentTool = "eraser";
+      tool.strokeStyle = "white";
+      console.log("eraser is clicked");
+      tool.lineWidth = 5;
+    } else if (toolName == "download") {
+      console.log("download clicked");
+      currentTool = "download";
+      downloadFile();
+    } else if (toolName == "sticky") {
+      currentTool = "sticky";
+      createSticky();
+    } else if (toolName == "upload") {
+      currentTool = "upload";
+      console.log(e.target);
+      uploadFile();
+    } else if (toolName == "undo") {
+      currentTool = "undo";
+      undoFN();
+    } else if (toolName == "redo") {
+      console.log("redo clicked");
+      redoFN();
+    }
+  });
+}
+
+//when holding the mouse
+canvas.addEventListener("mousedown", function (e) {
+  let sidx = e.clientX;
+  let sidy = e.clientY;
+  //drawing will start
+  tool.beginPath();
+  let toolbarheight = getYDelta();
+  tool.moveTo(sidx, sidy - toolbarheight);
+  isDrawing = true;
+});
+canvas.addEventListener("mousemove", function (e) {
+  if (isDrawing == false) return;
+  let eidx = e.clientX;
+  let eidy = e.clientY;
+  let toolbarheight = getYDelta();
+  tool.lineTo(eidx, eidy - toolbarheight);
+  tool.stroke();
+});
+canvas.addEventListener("mouseup", function (e) {
+  isDrawing = false;
+});
+//helper function
+let toolBar = document.querySelector(".toolbar");
+function getYDelta() {
+  let heightoftoolBar = toolBar.getBoundingClientRect().height;
+  return heightoftoolBar;
+}
